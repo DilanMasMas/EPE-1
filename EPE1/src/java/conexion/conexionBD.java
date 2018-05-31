cbe0c6a77b76960e26440697ea8d1a496fc923f7
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2018-05-30 23:33:40 -0400

    123

--- EPE1/src/java/conexion/conexionBD.java
+++ EPE1/src/java/conexion/conexionBD.java
@@ -7,6 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+//Aqui se establece la conexion en la base de datos 
+
 public class conexionBD {
 
     Connection conexion = null;
@@ -16,29 +18,4 @@ public class conexionBD {
     String url = "jdbc:mysql://localhost:3306/" + nombreBD;
     String usuario = "root";
     String password = "";
-    
-    public void insertarCliente(){
-    
-    try {
-           
-            Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection(
-                    url, usuario, password);
-            sentencia = conexion.createStatement();
-            
-            
-            String sql = "INSERT INTO clientes (`id_c`, `nombre`, `rut`, `direccion`, `libro_p`) VALUES (NULL, 'Harry Potter y la piedra filosofal', 'J. K. Rowling', 'Literatura fantástica', '3');";
-            resultados = sentencia.executeQuery(sql);
-
-            
-            sentencia.close();
-            conexion.close();
-
-        } catch (ClassNotFoundException | SQLException e) {
-
-            System.out.println("Error" + e);
-
-        }
-        
-    }
 }
